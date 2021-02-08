55469d3b6f18d5cd9678b3e6aefe18a38f8d7425
l-gorman <lgorman@turing.ac.uk>
2021-02-08 14:06:51 +0000

    modifying git ignore

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
+const mongoose = require('mongoose')
+
 const express = require('express')
 const app = express()
+
+
 
 // Define the port
 const port = 5000
 
-// Defining a get request and the response from this request
+//Middleware: A function that executes when "routes are being hit".
 
+//app.use('/form', () => {
+//   console.log("middleware running on the form components")
+//})
+
+
+// Defining a get request and the response from this request
 app.get('/', (req, res) => {
-  res.send('<h1>Legume-choice<h1>')
+  res.send('<h1>Legume-choice HOME<h1>')
 })
 
 
+app.get('/form', (req,res) => {
+  res.send('<h1>Legume-choice forms<h1>') 
+})
+
+// Make the app listen
 app.listen(port, () =>  {
 console.log(`Running on port ${port}`);
 })
+
+
+
