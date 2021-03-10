279a699bb4e77efc1f2ac1d5bb582e9af80ffe87
l-gorman <lgorman@turing.ac.uk>
2021-03-10 10:48:00 +0000

    reformatting with prettier

--- routes/projects.js
+++ routes/projects.js
@@ -7,32 +7,32 @@ const cors = require("cors");
 router.route("/get-projects/").get(async (req, res) => {
     try {
         //res.send("got projects");
-	result= await Projects.find();
-	res.send(result)
+        result = await Projects.find();
+        res.send(result);
     } catch (err) {
-        res.send("Error: "+ err);
+        res.send("Error: " + err);
     }
 });
 
 // Add a single project
 router.route("/submit-data").post(async (req, res) => {
     try {
-	const rawdata = req.body;
-	const username = req.body.user.username;
-	const projectID = req.body.projectInfo.projectID
-	const projectData = {
-	rawdata: rawdata,
-	username: username,
-	projectID:projectID
-	}
+        const rawdata = req.body;
+        const username = req.body.user.username;
+        const projectID = req.body.projectInfo.projectID;
+        const projectData = {
+            rawdata: rawdata,
+            username: username,
+            projectID: projectID,
+        };
         //const newProject = new Projects(req.body);
-        const newProject= new Projects(projectData);
-	newProject.save();
+        const newProject = new Projects(projectData);
+        newProject.save();
         //newUser.save();
         //res.send("User added");
         res.send("Project Added");
     } catch (err) {
-        res.send("Error: "+err);
+        res.send("Error: " + err);
     }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,69 +1,72 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("../models/Users");
-const cors = require("cors")
+const cors = require("cors");
 
-router.use(cors())
+router.use(cors());
 
 // Get all users
 router.route("/").get(async (req, res) => {
     //res.send('<h1>Legume-choice post<h1>')
-	try{
-	result = await Users.find({})
-	res.send(result)
-	}catch(err){
-	res.send('Error:' + err)
-	}
+    try {
+        result = await Users.find({});
+        res.send(result);
+    } catch (err) {
+        res.send("Error:" + err);
+    }
 });
 
-// Add a single user 
-router.route("/").post(async (req,res) => {
-	try{
-	const newUser = new Users(req.body)
-	newUser.save()
-	res.send('User added')
-	}catch(err){
-	res.send(err)
-	}
+// Add a single user
+router.route("/").post(async (req, res) => {
+    try {
+        const newUser = new Users(req.body);
+        newUser.save();
+        res.send("User added");
+    } catch (err) {
+        res.send(err);
+    }
 });
 
-
-// Add multiple single users 
-router.route("/insertmany").post(async (req,res) => {
-        try{
-	const newUsers = req.body.newUsers
-	result =  await Users.insertMany(newUsers)
-	res.send(result)
-	}catch(err){
-	res.send(err)	
-}       
+// Add multiple single users
+router.route("/insertmany").post(async (req, res) => {
+    try {
+        const newUsers = req.body.newUsers;
+        result = await Users.insertMany(newUsers);
+        res.send(result);
+    } catch (err) {
+        res.send(err);
+    }
 });
-
 
 // Delete a single User
 router.route("/delete/:UserId").post(async (req, res) => {
-	//const singleUser = await Users.findById(req.params.UserId);
-	try{
-	result = await Users.remove({_id:req.params.UserId})
-	res.send(result)
-	}catch(err){
-	res.send(err)
-	}
+    //const singleUser = await Users.findById(req.params.UserId);
+    try {
+        result = await Users.remove({ _id: req.params.UserId });
+        res.send(result);
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 // Delete multiple Users
 router.route("/deletemany").post(async (req, res) => {
-	try{
-	const deleteUsers = req.body.deleteUsers
-	const deleteEmails = deleteUsers.map((user)=> {return user.email})
-	const deleteUsernames = deleteUsers.map((user)=>{return user.username})
-	result = await Users.deleteMany({username: {$in: deleteUsernames},email: {$in: deleteEmails}})
-	res.send(result)
-	}catch(err){
-	res.send(err)
-	}
+    try {
+        const deleteUsers = req.body.deleteUsers;
+        const deleteEmails = deleteUsers.map((user) => {
+            return user.email;
+        });
+        const deleteUsernames = deleteUsers.map((user) => {
+            return user.username;
+        });
+        result = await Users.deleteMany({
+            username: { $in: deleteUsernames },
+            email: { $in: deleteEmails },
+        });
+        res.send(result);
+    } catch (err) {
+        res.send(err);
+    }
 });
-
-
 
 module.exports = router;
