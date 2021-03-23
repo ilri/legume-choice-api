c5442e1717599c4dd643b93537d144b50f0fbc54
l-gorman <lgorman@turing.ac.uk>
2021-03-23 17:23:00 +0000

    adding date to project archives

--- models/ProjectsArchive.js
+++ models/ProjectsArchive.js
@@ -4,6 +4,7 @@ const ProjectArchiveSchema = mongoose.Schema({
     rawdata: Map,
     projectSecret: String,
     projectID: String,
+    date: Date,
 });
 
 module.exports = mongoose.model("ProjectsArchive", ProjectArchiveSchema);
