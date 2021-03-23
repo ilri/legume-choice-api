36d7bc672be8b6f1d8e02ef9804bc0cb8025c565
lgorman <lgorman@turing.ac.uk>
2021-03-23 11:26:57 +0000

    getting rid of extra server functions

--- models/Users.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = mongoose.Schema({
-    username: String,
-    email: String,
-});
-
-module.exports = mongoose.model("Users", UserSchema);
