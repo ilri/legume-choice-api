42d6d07081470a0a19398d06157edf80cd6a41e4
l-gorman <lgorman@turing.ac.uk>
2021-03-31 13:45:37 +0100

    adding ability to add test projects and check for name dublication

--- routes/projects.js
+++ routes/projects.js
@@ -9,16 +9,16 @@ router.use(cors());
 
 router.options("*", cors());
 
-// Get project data
-router.route("/get-projects").get(async (req, res) => {
-    try {
-        //res.send("got projects");
-        result = await Projects.find();
-        res.send(result);
-    } catch (err) {
-        res.send("Error: " + err);
-    }
-});
+// // Get project data
+// router.route("/get-projects").get(async (req, res) => {
+//     try {
+//         //res.send("got projects");
+//         result = await Projects.find();
+//         res.send(result);
+//     } catch (err) {
+//         res.send("Error: " + err);
+//     }
+// });
 
 // Add a single project
 router.route("/submit-data/").post(async (req, res) => {
@@ -28,11 +28,13 @@ router.route("/submit-data/").post(async (req, res) => {
         const projectID = req.body.projectInfo.projectID;
         const projectSecret = req.body.projectSecret.secretKey;
         const date = new Date();
+        const projectName = req.body.projectInfo.projectName;
 
         // Merging the variables into a single object
         const projectData = {
             rawdata: rawdata,
             projectID: projectID,
+            projectName: projectName,
             projectSecret: projectSecret,
             date: date,
         };
@@ -43,6 +45,14 @@ router.route("/submit-data/").post(async (req, res) => {
         })
             .lean() // Lean extracts a simple javascript object, important for comparisons
             .exec();
+        const allProjects = await Projects.find({})
+            .lean() // Lean extracts a simple javascript object, important for comparisons
+            .exec();
+
+        const previousNames = [];
+        allProjects.map((project) => {
+            previousNames.push(project.rawdata.projectInfo.projectName);
+        });
 
         // If there is a previous project with the same ID:
         if (previousProject.length == 1) {
@@ -84,16 +94,26 @@ router.route("/submit-data/").post(async (req, res) => {
         // This is run in the case where this is not a pre-existing project
         if (previousProject.length == 0) {
             // Make sure there is a secret, then save the project
-            if (projectSecret !== undefined) {
-                const newProject = new Projects(projectData);
-                newProject.save();
-                res.send("Project Added");
+            if (previousNames.includes(projectName)) {
+                res.send(
+                    "Project name already exists. Try another name and submit again"
+                );
+            }
 
-                const newCursor = new Cursors({});
-                newCursor.save();
+            if (!previousNames.includes(projectName)) {
+                if (projectSecret !== undefined) {
+                    const newProject = new Projects(projectData);
+                    newProject.save();
+                    res.send("Project Added");
 
-                const newProjecttoArchive = new ProjectsArchive(projectData);
-                newProjecttoArchive.save();
+                    const newCursor = new Cursors({});
+                    newCursor.save();
+
+                    const newProjecttoArchive = new ProjectsArchive(
+                        projectData
+                    );
+                    newProjecttoArchive.save();
+                }
             }
 
             if (projectSecret === undefined) {
